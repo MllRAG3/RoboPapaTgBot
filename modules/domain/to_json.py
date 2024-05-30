c099f43540a6d3432b152eed03e1769e52541c86
v <t.shagin@gmail.com>
2024-05-30 11:49:33 +0300

    feat: Добавлена документация
    Исправлены ошибки:
    * -
    
    Добавлены:
    * Добавлена документация в класс ToJson
    
    Удалены:
    * -

--- modules/domain/to_json.py
+++ modules/domain/to_json.py
@@ -11,7 +11,17 @@ from typing import Callable
 
 class ToJson:
     def __init__(self):
-        self.TYPES: dict[str | Callable] = {
+        """
+        Аттрибуты:
+
+        TYPES (dict[[str, Callable]]):
+          Словарь с методом преобразования под каждый тип сообщения
+        jsons (tuple[str, str]):
+          Результаты преобразования
+        is_called (bool):
+          Был ли вызван объект класса (True если да)
+        """
+        self.TYPES: dict[str, Callable] = {
             'photo': self.photo_to_dict,
             'video': self.video_to_dict,
             'video_note': self.video_note_to_dict,
@@ -27,6 +37,10 @@ class ToJson:
 
     @staticmethod
     def photo_to_dict(message: Message) -> tuple[dict, dict | None]:
+        """
+        :param message: Информация о сообщении для преобразования
+        :return: Словарь со всеми важными данными для отправки фото
+        """
         data = {
             'photo': message.photo[0].file_id,
             'caption': message.caption,
@@ -37,6 +51,10 @@ class ToJson:
 
     @staticmethod
     def video_to_dict(message: Message) -> tuple[dict, dict | None]:
+        """
+        :param message: Информация о сообщении для преобразования
+        :return: Словарь со всеми важными данными для отправки видео
+        """
         data = {
             'video': message.video.file_id,
             'caption': message.caption,
@@ -47,6 +65,10 @@ class ToJson:
 
     @staticmethod
     def video_note_to_dict(message: Message) -> tuple[dict, dict | None]:
+        """
+        :param message: Информация о сообщении для преобразования
+        :return: Словарь со всеми важными данными для отправки видеосообщения
+        """
         data = {
             'data': message.video_note.file_id,
         }
@@ -55,6 +77,10 @@ class ToJson:
 
     @staticmethod
     def contact_to_dict(message: Message) -> tuple[dict, dict | None]:
+        """
+        :param message: Информация о сообщении для преобразования
+        :return: Словарь со всеми важными данными для отправки контакта
+        """
         data = {
             'phone_number': message.contact.phone_number,
             'first_name': message.contact.first_name,
@@ -65,6 +91,10 @@ class ToJson:
 
     @staticmethod
     def text_to_dict(message: Message) -> tuple[dict, dict | None]:
+        """
+        :param message: Информация о сообщении для преобразования
+        :return: Словарь со всеми важными данными для отправки текста
+        """
         data = {
             'text': message.text,
             'parse_mode': 'HTML'
@@ -74,6 +104,10 @@ class ToJson:
 
     @staticmethod
     def sticker_to_dict(message: Message) -> tuple[dict, dict | None]:
+        """
+        :param message: Информация о сообщении для преобразования
+        :return: Словарь со всеми важными данными для отправки стикера
+        """
         data = {
             'sticker': message.sticker.file_id
         }
@@ -82,6 +116,10 @@ class ToJson:
 
     @staticmethod
     def dice_to_dict(message: Message) -> tuple[dict, dict | None]:
+        """
+        :param message: Информация о сообщении для преобразования
+        :return: Словарь со всеми важными данными для отправки случайного эмодзи
+        """
         data = {
             'emoji': message.dice.emoji
         }
@@ -90,6 +128,10 @@ class ToJson:
 
     @staticmethod
     def voice_to_dict(message: Message) -> tuple[dict, dict | None]:
+        """
+        :param message: Информация о сообщении для преобразования
+        :return: Словарь со всеми важными данными для отправки голососвого сообщения
+        """
         data = {
             'voice': message.voice.file_id
         }
@@ -98,6 +140,10 @@ class ToJson:
 
     @staticmethod
     def document_to_dict(message: Message) -> tuple[dict, dict | None]:
+        """
+        :param message: Информация о сообщении для преобразования
+        :return: Словарь со всеми важными данными для отправки документа
+        """
         data = {
             'document': message.document.file_id,
             'caption': message.caption,
@@ -107,11 +153,19 @@ class ToJson:
         return data, util.extract_buttons(message)
 
     def __call__(self, message: Message) -> None:
+        """
+        :param message: Информация о сообщении для преобразования
+        присваивает аттрибуту self.jsons результат преобразований
+        :return:
+        """
         to_send, buttons = self.TYPES[message.content_type](message)
         self.jsons = json.dumps(to_send, ensure_ascii=False), json.dumps(buttons, ensure_ascii=False)
         self.is_called = True
 
     def __str__(self) -> str:
+        """
+        :return: Текст для сообщения с результатом (Необходимо отформатировать по HTML!)
+        """
         if not self.is_called:
             raise NotImplementedError('Перед преобразованием в строку необходимо вызвать объект класса!')
 
