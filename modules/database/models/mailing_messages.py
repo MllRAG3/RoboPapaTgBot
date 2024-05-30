b71629bf3fa68d3f0c0c766ec1a96bffa5ebe4b0
v <t.shagin@gmail.com>
2024-05-30 10:06:49 +0300

    feat: Добавлена документация
    Исправлены ошибки:
    * -
    
    Добавлены:
    * Документация к моделям базы данных
    
    Удалены:
    * -

--- modules/database/models/mailing_messages.py
+++ modules/database/models/mailing_messages.py
@@ -4,7 +4,18 @@ from modules.database.models.base import BaseModel
 
 class MailingMessages(BaseModel):
     """
-    Таблица с информацией о рассылках
+    Таблица с информацией о рассылках (Модель базы данных tg_bot_database)
+
+    Аттрибуты:
+
+    type (str):
+      Тип сообщения
+    send_data_json (str):
+      JSON-словарь с данными о сообщении (все кроме кнопок)
+    send_data_buttons_json (str):
+      JSON-словарь с данными о кнопках (объектах telebot.types.InlineKeyboardMarkup)
+    is_active (bool):
+      Активная ли запись (True если рассылается)
     """
     type = CharField(max_length=16)
     send_data_json = CharField(max_length=4096)
